8968e0279c60be9062178e2fd701585249238ea5
Karoll454 <113633795+Karoll454@users.noreply.github.com>
2024-04-29 12:04:04 -0300

    Update main.js

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-4-04T00:00:00");
-const tempoObjetivo2 = new Date("2023-03-03T00:00:00");
-const tempoObjetivo3 = new Date("2022-02-02T00:00:00");
-const tempoObjetivo4 = new Date("2021-01-01T00:00:00");
+const tempoObjetivo1 = new Date("2025-4-04T00:00:00");
+const tempoObjetivo2 = new Date("2025-03-03T00:00:00");
+const tempoObjetivo3 = new Date("2025-02-02T00:00:00");
+const tempoObjetivo4 = new Date("2025-01-01T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
